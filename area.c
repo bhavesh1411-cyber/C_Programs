592af206c6323848321b53616ab0e60821dc980b
Bhavesh <gbhavesh1411@gmail.com>
2026-08-14 19:23:03 +0000

    update projects

--- area.c
+++ area.c
@@ -3,13 +3,18 @@
 float main()
 {
     float lenght, breadth, area, perimeter;
+
     printf("Enter the lenght of the rectangle:");
     scanf("%f", &lenght);
+
     printf("Enter the breadth of the rectangle:");
     scanf("%f", &breadth);
+
     area = lenght * breadth;
     perimeter = 2 * (lenght + breadth);
+
     printf("Area of the rectangle of the given length and breadth is %.2f\n", area);
     printf("The Perimeter of the rectangle of the given length and breadth is %.2f\n", perimeter);
+
     return 0;
 }
